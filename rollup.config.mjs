95e434a010e70da0236b077bb0ea8d416ccb9ea6
diaslucia <luciadias.dev@gmail.com>
2024-04-24 16:23:53 -0300

    RESOLVE css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-/* import postcss from "rollup-plugin-postcss"; */
+import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
-import scss from "rollup-plugin-scss";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default [
   {
@@ -28,14 +28,20 @@ export default [
         tsconfig: "./tsconfig.json",
         exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
       }),
-      /*   postcss({ extensions: [".css"], inject: true, extract: false }), */
-      scss(),
+      postcss({
+        extensions: [".css"],
+        inject: false,
+        extract: true,
+      }),
+      nodeResolve({
+        extensions: [".css"],
+      }),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+    external: [/\.css$/u],
   },
 ];
